5bd6c785f61830ee5b3a2cabb9c53e7007248854
denaumenko <denaumenko@gmail.com>
2021-02-09 00:20:13 -0800

    FirstModelWasCreated

---
+++ Models/UserVocabulary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnglishVocabulary.Models
+{
+    public class UserVocabulary
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+
+        public string Definiton { get; set; }
+    }
+}
